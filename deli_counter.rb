936361d33834493fb211913c884af1e048d4a183
Aruni Ranaweera <arunixr@gmail.com>
2019-04-01 11:48:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+def line(katz_deli)
+  if katz_deli==[]
+    return "The line is currently empty."
+  else
+    i=1
+    print "The line is currently: "
+      katz_deli.length.times do
+      print "#{i}. #{katz_deli[i-1]} "
+      i=i+1
+      end
+  end
+puts
+end
